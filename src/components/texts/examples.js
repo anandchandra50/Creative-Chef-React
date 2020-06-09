c61b29d014e32d4b67cd2a83ee6b55a3f59d9da1
Anand Chandra <anandchandra50@gmail.com>
2020-06-08 17:30:05 -0700

    Added cheatsheet images

--- src/components/texts/examples.js
+++ src/components/texts/examples.js
@@ -12,7 +12,7 @@ export default function Examples(props) {
         Hopefully, the theory we’ve covered makes sense to you. Becoming an independent chef will take time and practice, and <span className="bold">to help you get started, we’ve created two cheatsheets:</span> flavor profiles, and cooking methods.<br/><br/>
         These are meant as a starting point to create dishes without a recipe—we hope you can eventually create your own flavor combinations and move away from these cheatsheets altogether.<br/><br/>
         A few notes: add seasoning slowly, and taste frequently. Our ratios are estimates, and should be adjusted for each dish.<br/><br/>
-        <span className="italic">These are designed for easy access on your phone.</span><br/><br/>
+        <span className="italic">These are designed for easy access on your phone. Click to download.</span><br/><br/>
         {isMobile ?
           <>
             View: <a className="link" href={flavorCheatsheet1} target="_blank">Flavor Profiles 1</a><br/>
@@ -21,9 +21,12 @@ export default function Examples(props) {
           </>
           :
           <>
-            Download: <a className="link" href={flavorCheatsheet1} download>Flavor Profiles 1</a><br/>
-            Download: <a className="link" href={flavorCheatsheet2} download>Flavor Profiles 2</a><br/>
-            Download: <a className="link" href={cookingMethodsCheatsheet} download>Cooking Methods</a>
+            Flavor Profiles:
+            <a href={flavorCheatsheet1} download><img className="cheatsheet" src={flavorCheatsheet1} alt="Flavor Cheatsheet 1"/></a><br/>
+            <a href={flavorCheatsheet2} download><img className="cheatsheet" src={flavorCheatsheet2} alt="Flavor Cheatsheet 2"/></a><br/>
+            <br/>
+            Cooking Methods:
+            <a href={cookingMethodsCheatsheet} download><img className="cheatsheet" src={cookingMethodsCheatsheet} alt="Cooking Methods Cheatsheet"/></a><br/>
           </>
         }
       </div>
